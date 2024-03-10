b9ab2bd974d63283b4e73450c8a514673271d379
Golubev Artem <109762929+g0lubenk0@users.noreply.github.com>
2024-03-10 15:47:46 +0300

    update App, Differ, TestDiffer, TestParser

--- app/src/test/java/hexlet/code/TestParser.java
+++ app/src/test/java/hexlet/code/TestParser.java
@@ -8,7 +8,8 @@ import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestParser {
     private final String filepath1 =
