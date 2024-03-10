eb4bc7867fe7ab003327ce314c9df2fc43f94677
Golubev Artem <109762929+g0lubenk0@users.noreply.github.com>
2024-03-10 18:02:36 +0300

    Revert "update formatters"
    This reverts commit 01dd0feaf1652381718c5863c8f51c811e76eed8.

--- app/src/main/java/hexlet/code/formatters/Plain.java
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -6,7 +6,10 @@ import java.util.List;
 import java.util.Map;
 
 public class Plain implements Formatter {
-    public StringBuilder appendDifference(List<String> keyList, Map<String, Object> map1, Map<String, Object> map2) {
+    public StringBuilder appendDifference(
+            List<String> keyList,
+            Map<String, Object> map1,
+            Map<String, Object> map2) {
         var builder = new StringBuilder();
         for (var key: keyList) {
             if (map1.containsKey(key) && map2.containsKey(key)) {
@@ -15,15 +18,28 @@ public class Plain implements Formatter {
                 if (value1 == null && value2 == null) {
                     break;
                 } else {
-                    builder.append("Property '").append(key).append("' was updated. From ").append(value1)
-                            .append(" to ").append(value2).append("\n");
+                    builder
+                            .append("Property '")
+                            .append(key)
+                            .append("' was updated. From ")
+                            .append(value1)
+                            .append(" to ")
+                            .append(value2)
+                            .append("\n");
                 }
             } else if (map1.containsKey(key) && !map2.containsKey(key)) {
-                builder.append("Property '").append(key).append("' was removed.").append("\n");
+                builder
+                        .append("Property '")
+                        .append(key)
+                        .append("' was removed.")
+                        .append("\n");
             } else {
                 builder
-                        .append("Property '").append(key).append("' was added wtih value: ")
-                        .append(Formatter.valueToString(map2.get(key))).append("\n");
+                        .append("Property '")
+                        .append(key)
+                        .append("' was added wtih value: ")
+                        .append(Formatter.valueToString(map2.get(key)))
+                        .append("\n");
             }
         }
         return builder;
